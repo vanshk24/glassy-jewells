4f388b6e8a4f96be21bdaf1fac7dda9e75cce53d
Vansh <vanshkumarbhurani4@gmail.com>
2026-01-03 21:55:33 +0530

    Fix SSR server build and express types

--- api/server.ts
+++ api/server.ts
@@ -1,15 +1,20 @@
-import { createRequestHandler } from "@react-router/express";
 import express from "express";
-import * as build from "../build/server/index.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.all(
-  "*",
-  createRequestHandler({
-    build,
-    mode: process.env.NODE_ENV
-  })
+const serverBuildPath = path.join(
+  __dirname,
+  "../build/server/index.js"
 );
+
+// Dynamic import (important)
+const serverBuild = await import(serverBuildPath);
+
+app.all("*", serverBuild.default);
 
 export default app;
