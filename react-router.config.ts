2492b8a8947ac284dde38fc1f45742afe76631c1
Vansh <vanshkumarbhurani4@gmail.com>
2026-01-03 21:42:20 +0530

    Add Vercel SSR config

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
+  appDirectory: "app",
+  buildDirectory: "build",
   ssr: true,
-  future: {
-    unstable_optimizeDeps: true,
-  },
 } satisfies Config;
